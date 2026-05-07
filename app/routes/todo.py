d3e53fbbc88b931fed1cc41bf7801718871d5023
Benzzo Krugger <benzzo.krugger@proton.me>
2026-05-07 20:25:47 +0200

    feat: add todo

--- app/routes/todo.py
+++ app/routes/todo.py
@@ -1,13 +1,29 @@
 # FastAPI imports
-from fastapi import Request, APIRouter
+from fastapi import Request, APIRouter, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from typing import Annotated
+from uuid import uuid4
 
 router = APIRouter()
 
-templates = Jinja2Templates('app/templates')
+templates = Jinja2Templates("app/templates")
+
+todos = []
+
 
 # Routes
 @router.get("/", response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse(request=request, name='index.html.j2')
+    return templates.TemplateResponse(request=request, name="index.html.j2")
+
+
+@router.post("/todos", response_class=HTMLResponse)
+def add_todo(request: Request, title: Annotated[str, Form()]):
+
+    todo = {"id": uuid4(), "title": title, "completed": False}
+    todos.append(todo)
+
+    return templates.TemplateResponse(
+        request=request, name="/partials/todo_item.html.j2", context={"todo": todo}
+    )
