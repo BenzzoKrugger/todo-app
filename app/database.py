2141e4299bbcb5734606bb06aa32e985cb2ad647
Benzzo Krugger <benzzo.krugger@proton.me>
2026-05-16 13:11:47 +0200

    feat: create database.py

---
+++ app/database.py
@@ -0,0 +1,11 @@
+from sqlmodel import create_engine, Session, SQLModel
+from .models.todo import Todo
+
+engine = create_engine("sqlite:///database.db", connect_args={'check_same_thread': False})
+
+def create_db_and_tables():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session
